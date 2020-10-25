432247b0ada449814786f2036f71e434d0dbb983
sebnorth <sebnorth@gmail.com>
2020-10-25 17:38:10 +0100

    New programs: if-else, switch, goto clauses.

---
+++ EvenUneven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenUneven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int.TryParse(Console.ReadLine(), out int number);
+
+            if (number % 2 == 0)
+            
+                Console.WriteLine("The number {0} is even.", number);
+            
+            else
+            
+
+                Console.WriteLine("The number {0} is odd.", number);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SignOfANumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number, please: ");
+            int.TryParse(Console.ReadLine(), out int a);
+
+            if (a > 0) Console.WriteLine("The number {0} is positive", a);
+
+            else
+            {
+
+                if (a < 0) Console.WriteLine("The number {0} is negative", a);
+
+                else Console.WriteLine("You entered zero.");
+
+            }
+
+        }
+    }
+}
